69897e1e441a1f0826b6f62ef548b39a704d736e
Tch1b0 <Jpour3006@gmail.com>
2021-10-19 18:23:04 +0200

    Define `LENGTH` as a constant

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include "head.h"
+#define LENGTH 10
 
 int main() {
-    int test[3] = {1, 5, 3};
-    prettyPrint(bubbleSort(test));
+    int test[LENGTH] = {5, 3, 6, 1, 7, 0, 4, 2, 9, 8};
+
+    printf("Unsorted:\n");
+    prettyPrint(test, LENGTH);
+
+    printf("\n\nSorted:\n");
+    prettyPrint(bubbleSort(test, LENGTH), LENGTH);
     return 0;
 }
